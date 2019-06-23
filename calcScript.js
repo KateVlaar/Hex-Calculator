66b02c8583e0f0f1a43e7dddc4be58dcd53bbcb9
Kate Vlaar <kate.vlaar@shaw.ca>
2019-06-23 15:42:32 -0600

    evaluate base-10 expressions with math.js

--- calcScript.js
+++ calcScript.js
@@ -3,22 +3,18 @@ $(".btn").click(function(event) {
 	console.log("you clicked: ", thingClicked)
 	var dispValue = document.getElementById("displayValue").innerHTML;
 	
-	if(["+", "-", "/", "*"].includes(dispValue[dispValue.length - 1])) {
-		console.log('here')
-		$('#displayValue').html(' ' + dispValue + ' ' + thingClicked)
+	if(["+", "-", "/", "*"].includes(thingClicked)) {
+		$('#displayValue').html(' ' + dispValue + ' ' + thingClicked + ' ')
 	} else {
 		$('#displayValue').html(dispValue + thingClicked)
 	}
 })
 
 $(".enter").click(function(event) {
-	var thingClicked = this.innerHTML
-	console.log("you clicked: ", thingClicked)
-	if(document.getElementById("displayValue").innerHTML == null) {
-		$('#displayValue').html(thingClicked)
-	} else {
-		$('#displayValue').html(document.getElementById("displayValue").innerHTML + ' ' + thingClicked)
-	}
+	var toCalculate = document.getElementById("displayValue").innerHTML
+	var result = math.evaluate(toCalculate)
+	document.getElementById("displayValue").innerHTML = result;
+	// console.log(toCalculate)
 })
 
 
